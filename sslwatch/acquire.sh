fa1d297d06f291b76ac29686148365a0e0c3dce6
nra4ever <muerteproductions@gmail.com>
2023-04-06 02:22:59 +0100

    f

--- sslwatch/acquire.sh
+++ sslwatch/acquire.sh
@@ -16,20 +16,22 @@ interval=3600
 # Set the destination path for the remote key file
 remote_key_file="/etc/nginx/ssl/$subdomain.guth3d.com/"
 
-while true; do
-    # Get the latest     version
-    version_output=$(curl http://$server_name:8783/get_version/$subdomain)
-    latest_ver=$(echo "$version_output" | grep -o "\"version\":.*" | sed -e 's/^.*: //' -e 's/[},]*$//')
+echo $(ssh-add -l)
 
-    # Check if the version has changed
-    if [[ $latest_ver != $curr_ver ]]; then
-        # Copy the SSL keys to the local machine using SCP
-        rsync -Pav -e "ssh -i ~/.ssh/id_rsa" debian@$server_name:$remote_key_file /ssl/
-        # Update the current version
-        curr_ver=$latest_ver
-        echo $latest_ver > /version
-        echo "SSL keys updated from $server_name"
-    fi
-    # Wait for the specified interval
-    sleep $interval
-done
+# while true; do
+#     # Get the latest     version
+#     version_output=$(curl http://$server_name:8783/get_version/$subdomain)
+#     latest_ver=$(echo "$version_output" | grep -o "\"version\":.*" | sed -e 's/^.*: //' -e 's/[},]*$//')
+
+#     # Check if the version has changed
+#     if [[ $latest_ver != $curr_ver ]]; then
+#         # Copy the SSL keys to the local machine using SCP
+#         rsync -Pav -e "ssh -i ~/.ssh/id_rsa" debian@$server_name:$remote_key_file /ssl/
+#         # Update the current version
+#         curr_ver=$latest_ver
+#         echo $latest_ver > /version
+#         echo "SSL keys updated from $server_name"
+#     fi
+#     # Wait for the specified interval
+#     sleep $interval
+# done
