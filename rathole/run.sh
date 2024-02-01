2ae9f732c0ba2cc005052a2487921275bef562a0
nra4ever <muerteproductions@gmail.com>
2024-02-01 23:22:15 +0000

    changing to noise protocol

--- rathole/run.sh
+++ rathole/run.sh
@@ -2,12 +2,9 @@
 CLIENT_ARCH=$(apk --print-arch)
 REMOTE_ADDRESS=$(bashio::config 'remote_address')
 LOCAL_ADDRESS=$(bashio::config 'local_address_ssl')
-LOCAL_ADDRESS_INSEC=$(bashio::config 'local_address_http')
 TOKEN=$(bashio::config 'token')
-CA_FILE=$(bashio::config 'trusted_root')
-HOSTNAME=$(bashio::config 'hostname')
+KEY=$(bashio::config 'public_key')
 NAME1=$(bashio::config 'name_ssl')
-NAME2=$(bashio::config 'name_http')
 
 echo "[client]
 remote_addr = \"$REMOTE_ADDRESS\"
@@ -16,17 +13,14 @@ default_token = \"$TOKEN\"
 [client.services.$NAME1]
 local_addr = \"$LOCAL_ADDRESS\"
 
-[client.services.$NAME2]
-local_addr = \"$LOCAL_ADDRESS_INSEC\"
-
 [client.transport]
-type = \"tls\"
+type = \"noise\"
 
-[client.transport.tls]
-trusted_root = \"/ssl/$CA_FILE\" 
- hostname = \"$HOSTNAME\" " > /rathole-$CLIENT_ARCH/client.toml
+[client.transport.noise]
+remote_public_key = \"$KEY\" " > /rathole-$CLIENT_ARCH/client.toml
 
 cd /rathole-$CLIENT_ARCH
 
 chmod +x rathole
 ./rathole client.toml
+
